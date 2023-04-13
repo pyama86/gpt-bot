eae351835aa1991c34b611f63c7c917e62d265f5
pyama <pyama@pepabo.com>
2023-04-13 19:28:25 +0900

    support app

--- main.py
+++ main.py
@@ -26,20 +26,16 @@ def base_url():
         base_url = os.environ.get("GITHUB_API").rstrip("/")
     return base_url
 
-def get_token_with_type():
+def get_token_with_type(org, repo):
     if (os.environ.get("GITHUB_APP_ID") is not None and
-        os.environ.get("GITHUB_APP_INSTALLATION_ID") is not None and
         os.environ.get("GITHUB_APP_PRIVATE_KEY") is not None):
         GITHUB_APP_ID = os.environ.get("GITHUB_APP_ID")
         PRIVATE_KEY = os.environ.get("GITHUB_APP_PRIVATE_KEY")
-        INSTALLATION_ID = os.environ.get("GITHUB_APP_INSTALLATION_ID")
-
-        with open(PRIVATE_KEY_PATH, "rb") as key_file:
-            PRIVATE_KEY = key_file.read()
-
         jwt_token = create_jwt(GITHUB_APP_ID, PRIVATE_KEY)
         integration = GithubIntegration(integration_id=GITHUB_APP_ID, private_key=PRIVATE_KEY, base_url=base_url())
-        install_token = integration.get_access_token(INSTALLATION_ID).token
+        installation_id = integration.get_installation(org, repo).id
+
+        install_token = integration.get_access_token(installation_id).token
         return ["Bearer", install_token]
     return ["token", os.environ.get("GITHUB_TOKEN")]
 
@@ -52,9 +48,7 @@ def ok():
 
 @webhook.hook("issue_comment")
 def on_issue_comment(data):
-    token_type, token = get_token_with_type()
-    precontext = ""
-    content = ""
+    query = ""
     if (
             "@gpt-bot" in data['comment']['body'] and
             (
@@ -66,6 +60,7 @@ def on_issue_comment(data):
             )
     ):
         print("GPT-BOTのコメントを検知しました。{}".format(data['comment']['body']))
+        token_type, token = get_token_with_type(data['organization']['login'], data['repository']['name'])
 
         context = '''
         ## 入力仕様
@@ -79,7 +74,7 @@ def on_issue_comment(data):
         - あなたが推敲する必要がない、リファクタリングする必要がない場合は、推薦の必要がないと述べたあとに、入力内容を解説してください。。
 
         ## 入力
-        {content}
+        {query}
         '''
 
         client = Github(login_or_token=token, base_url=base_url())
@@ -88,7 +83,7 @@ def on_issue_comment(data):
         input_text = ""
         if "@gpt-bot /comment" in data['comment']['body']:
             input_text = 'GitHubで生成されたIssueのコメントを入力します。'
-            content = data['comment']['body'].replace("@gpt-bot /comment", "")
+            query = data['comment']['body'].replace("@gpt-bot /comment", "")
             issue = repo.get_issue((data['issue']['number']))
         elif "@gpt-bot /pr" in data['comment']['body'] and data['issue']['pull_request'] is not None:
             input_text = 'GitHubで生成されたPull Requestのdiffを入力します'
@@ -100,23 +95,22 @@ def on_issue_comment(data):
 
             diff = requests.get(data['issue']['pull_request']['url'], headers=headers)
             diff.raise_for_status()
-            content = diff.text
+            query = diff.text
 
             pull_request = repo.get_pull(data['issue']['number'])
             issue = pull_request.as_issue()
         else:
             return
 
-        if(len(content)> 8000):
-            content = content[:8000]
+        if(len(query)> 8000):
+            query = query[:8000]
             issue.create_comment("コンテンツが長すぎるので、コンテンツを切り詰めました")
 
         print("send request to openapi")
         response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=[
-                {"role": "user", "content": precontext},
-                {"role": "user", "content": textwrap.dedent(context).format(content=content, input_text=input_text)},
+                {"role": "user", "content": textwrap.dedent(context).format(query=query, input_text=input_text)},
             ]
         )
 
@@ -132,5 +126,5 @@ def on_issue_comment(data):
         issue.create_comment(comment)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=10080)
+    app.run(host="0.0.0.0", port=10080)
 
